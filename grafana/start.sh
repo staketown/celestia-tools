692b8cc39d5a5192453ecc10d454cbe2637cb651
Yevhen Sydorenko <mogoll92@gmail.com>
2024-04-17 18:02:13 +0300

    Update alertmanager to use discord for alerting

--- grafana/start.sh
+++ grafana/start.sh
@@ -17,7 +17,7 @@ sed -i "s/PUBLIC_IP/$(curl -s ifconfig.me)/g" conf/prometheus/prometheus.yml
 sed -i "s/OTEL_PROMETHEUS_EXPORTER/${OTEL_PROMETHEUS_EXPORTER}/g" conf/prometheus/prometheus.yml
 
 cp conf/alertmanager.yaml.tpl conf/alertmanager.yaml
-sed -i "s/PD_SERVICE_KEY/${PD_INTEGRATION_KEY}/g" conf/alertmanager.yaml
+sed -i "s/DISCORD_WEB_HOOK/${DISCORD_WEB_HOOK}/g" conf/alertmanager.yaml
 
 cp conf/otel-collector-config.yaml.tpl conf/otel-collector-config.yaml
 sed -i "s/OTEL_PROMETHEUS_EXPORTER/${OTEL_PROMETHEUS_EXPORTER}/g" conf/otel-collector-config.yaml
